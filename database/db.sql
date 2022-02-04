9f57ffe32401d0c7f126135c99bc8b1f7cf5d673
Paola Rivas <paoa.rivasm@gmail.com>
2022-02-04 18:25:41 -0400

    db.sql file modification

--- database/db.sql
+++ database/db.sql
@@ -20,3 +20,15 @@ CREATE TABLE access (
   CONSTRAINT fk_business FOREIGN KEY (business_id) REFERENCES business(id),
   CONSTRAINT fk_users FOREIGN KEY (user_id) REFERENCES users(id)
 );
+
+CREATE TABLE [dbo].[usuarios](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[username] [varchar](100) NOT NULL,
+	[fullname] [varchar](45) NOT NULL,
+	[password] [varchar](100) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
